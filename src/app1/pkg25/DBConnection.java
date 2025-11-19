ebbc68c9086508d36845fa1ac4cd10ecace28d45
mstf-ai <albrnsmstfy252@gmail.com>
2025-11-19 14:04:39 +0200

    M

---
+++ src/app1/pkg25/DBConnection.java
@@ -0,0 +1,21 @@
+package app1.pkg25;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mckawy_demo1";
+    private static final String USER = "mostafa";
+    private static final String PASSWORD = "Mostafa123";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
